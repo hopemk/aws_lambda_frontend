052753be0ed689db7a81aa7df56164ca62dd9ac6
Hope Kugotsi <hopekugotsi@gmail.com>
2022-03-03 09:35:18 +0200

    first commit

---
+++ contact/pages/contactform.js
@@ -0,0 +1,33 @@
+export default function(){
+    return (
+        <div>
+            <div class="form">
+                    <form>
+                        <div>
+                            <input type="text" class="" id=""/>
+                        </div>
+                        <div>
+                            <input type="text" class="" id=""/>
+                        </div>
+                        <div>
+                            <input type="text" class="" id=""/>
+                        </div>
+                        <div>
+                            <input type="text" class="" id=""/>
+                        </div>
+                        <div class="textarea">
+                            <textarea rows="4" type="text" class="" id=""></textarea>
+                        </div>
+                        <div class="send-message">
+                            <a class="about-button"  stylehref="#" role="button">
+                                <span>view more</span>
+                                
+                            </a>
+                        </div>
+                    </form>
+                </div>
+        </div>
+    )
+        
+    
+}
