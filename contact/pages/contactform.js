3da55b278299f1a944abdf26ce0de9d3c7fd61c4
Hope Kugotsi <hopekugotsi@gmail.com>
2022-03-04 12:10:59 +0200

    changes

--- contact/pages/contactform.js
+++ contact/pages/contactform.js
@@ -1,33 +1,62 @@
-export default function(){
+import React, { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import axios from 'axios';
+
+export default function () {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        message: '',
+        subject: '',
+        contact: ''
+    })
+    const handleInputChange = (event) => {
+        event.preventDefault();
+        setFormData({
+            ...formData, [event.target.name]: event.target.value
+        })
+        console.log(formData)
+    }
+    const handleSubmit = event => {
+        event.preventDefault();
+
+        axios.post(`https://9d0t51qr2m.execute-api.us-east-2.amazonaws.com/Stage/contact-HelloWorldFunction-p1kRjNLaBiK9`, { formData })
+            .then(res => {
+                console.log(res);
+                console.log(res.data);
+            })
+    }
     return (
-        <div>
-            <div class="form">
-                    <form>
-                        <div>
-                            <input type="text" class="" id=""/>
-                        </div>
-                        <div>
-                            <input type="text" class="" id=""/>
-                        </div>
-                        <div>
-                            <input type="text" class="" id=""/>
-                        </div>
-                        <div>
-                            <input type="text" class="" id=""/>
-                        </div>
-                        <div class="textarea">
-                            <textarea rows="4" type="text" class="" id=""></textarea>
-                        </div>
-                        <div class="send-message">
-                            <a class="about-button"  stylehref="#" role="button">
-                                <span>view more</span>
-                                
-                            </a>
-                        </div>
-                    </form>
-                </div>
+        <div className={styles.container}>
+            <main className={styles.main}>
+
+                <form className={styles.form}>
+                    <div>
+                        <input type="text" className={styles.input} id="name" name="name" onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <input type="text" className={styles.input} id="email" name="email" onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <input type="text" className={styles.input} id="contact" name="contact" onChange={handleInputChange} />
+                    </div>
+                    <div>
+                        <input type="text" className={styles.input} id="subject" name="subject" onChange={handleInputChange} />
+                    </div>
+                    <div className={styles.textarea}>
+                        <textarea rows="4" type="text" className={styles.textarea_input} id="message" name="message" onChange={handleInputChange}></textarea>
+                    </div>
+                    <div className="send-message">
+                        <a className={styles.about_button} stylehref="#" role="button" onClick={handleSubmit}>
+                            <span>view more</span>
+
+                        </a>
+                    </div>
+                </form>
+
+            </main>
         </div>
     )
-        
-    
+
+
 }
--- contact/pages/getonemessage.js
+++ contact/pages/getonemessage.js
@@ -1,3 +1,17 @@
+import React, { useState, useEffect } from "react";
+import styles from '../styles/Home.module.css'
 export default function(){
+    const [messages, setMessages] = useState(0)
+    const getMessages = () =>{
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(res => {
+           const data = res.data
+          setMessages({ data });
+        })
+    }
+    useEffect(() => {
+        getMessages();
+    })
+    return "data"
     
 }
